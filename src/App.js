e382aedd16afe71b835dfe3f95a39239e664b21f
kafuikumah <kafui.kofi.kumah@gmail.com>
2023-01-03 16:03:08 +0000

    Rendering all movies based on a single search item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,69 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import MovieCard from './MovieCard';
+import './App.css';
+import SearchIcon from './search.svg'
+
+ 
+
+const API_URL = 'http://www.omdbapi.com?apikey=4c9da28b';
+
+// const movie1 = {
+//   "Title": "Batman v Superman: Dawn of Justice (Ultimate Edition)",
+//   "Year": "2016",
+//   "imdbID": "tt18689424",
+//   "Type": "movie",
+//   "Poster": "https://m.media-amazon.com/images/M/MV5BN2I4OTllM2MtMWVhNC00MjkzLWJlMDUtN2FhMGQ2ZGVjMjllXkEyXkFqcGdeQXVyMTEyNzgwMDUw._V1_SX300.jpg"
+// };
 
 const App = () => {
 
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+
+  }
+
+  useEffect( () =>{
+    searchMovies('Superman');
+
+  }, []);
+
   return (
-    <div id='root'>
-      <h1>App</h1>  
+    <div className='app'>
+      <h1>Lindol </h1>
+      <div className='search'>
+        <input
+          placeholder=' Search for movies'
+          value='Superman'
+          onChange={() => {}}
+        />
+        <img
+          src={SearchIcon}
+          alt='search'
+          onChange={() => {}}
+        />
+      </div>
+
+      {
+        movies?.length > 0
+        ? (
+          <div className='container'>
+            {movies.map((movie) => (
+              <MovieCard movie = {movie} />
+            ))}
+          </div>
+        ) : (
+              <div className='empty'>
+                <h2> No movies found</h2>
+              </div>
+        )
+      }
+      
     </div>
     
   )
